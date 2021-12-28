6e0a9d8ed9b060edb502b57166a53d33b6e47be6
suzuki1108 <suzukinoko.1108@icloud.com>
2021-12-29 00:22:27 +0900

    Home画面で使用するボタンコンポーネント作成

---
+++ src/components/HomeScreenButton.vue
@@ -0,0 +1,18 @@
+<template>
+  <router-link
+    :to="url"
+    class="text-center w-40 py-5 bg-primary opacity-80 font-bold rounded hover:opacity-100"
+  >
+    <slot></slot>
+  </router-link>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "HomeScreenButton",
+  props: {
+    url: String,
+  },
+});
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,23 @@
 <template>
   <div class="home relative w-full min-h-screen bg-cover bg-no-repeat">
     <header-component :is_bg_clear="true" />
+    <div class="flex justify-center gap-5 md:gap-40">
+      <home-screen-button url="/">おまかせ検索</home-screen-button>
+      <home-screen-button url="/">詳細検索</home-screen-button>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 import HeaderComponent from "@/components/HeaderComponent.vue";
+import HomeScreenButton from "@/components/HomeScreenButton.vue";
 
 export default defineComponent({
   name: "Home",
   components: {
     HeaderComponent,
+    HomeScreenButton,
   }
 });
 </script>
