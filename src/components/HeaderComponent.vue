3de9e4930b8d18e0dfff200fcb86fceda5d42ca1
suzuki1108 <suzukinoko.1108@icloud.com>
2021-12-28 23:27:19 +0900

    ヘッダーコンポーネントの作成

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,28 @@
+<template>
+  <header
+    class="flex justify-between text-white font-bold font-body items-center"
+    :class="is_bg_clear ? '' : 'bg-primary'"
+  >
+    <div>
+      <h1 class="mt-4 mb-4 ml-8 text-lg md:text-2xl">nanitabe</h1>
+    </div>
+    <div>
+      <a href="#" class="mr-8 text-xs md:text-base">ログイン</a>
+      <a href="#" class="mr-8 text-xs md:text-base">無料会員登録</a>
+    </div>
+  </header>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "HeaderComponent",
+  props: {
+    // ヘッダーの背景を透明にするかプライマリカラーにするか判別する
+    is_bg_clear: {
+      type: Boolean,
+      required: true,
+    },
+  },
+});
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,18 @@
 <template>
   <div class="home relative w-full min-h-screen bg-cover bg-no-repeat">
-    <h1 class="font-body text-4xl text-white">nanitabe font test</h1>
+    <header-component :is_bg_clear="true" />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import HeaderComponent from "@/components/HeaderComponent.vue";
 
 export default defineComponent({
   name: "Home",
+  components: {
+    HeaderComponent,
+  }
 });
 </script>
 
