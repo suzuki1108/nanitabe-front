79aef76d27daaf3d36afbce73979b01acddf695e
suzuki1108 <suzukinoko.1108@icloud.com>
2021-12-31 17:20:51 +0900

    検索結果タイトル作成

--- src/views/LocationSearchResult.vue
+++ src/views/LocationSearchResult.vue
@@ -1,5 +1,25 @@
 <template>
   <header-component :is_bg_clear="false" />
+  <div class="container mx-auto">
+    <div class="mt-8 flex flex-col items-center md:flex-row md:justify-between">
+      <div class="flex items-baseline">
+        <h2 class="font-bold text-base md:text-xl md:ml-4">
+          現在地からの検索結果
+        </h2>
+        <p class="ml-2 md:ml-5 text-xs">
+          <span class="font-bold">1~10件</span>を表示
+        </p>
+      </div>
+      <div>
+        <button
+          class="text-sm text-darkgray mt-2 w-64 h-8 md:mr-4 md:mt-0 md:w-40 bg-secondary rounded-lg shadow-lg hover:opacity-80"
+        >
+          ルーレットで決める
+        </button>
+      </div>
+    </div>
+    <div class="mt-2 border border-lightgray"></div>
+  </div>
 </template>
 
 <script lang="ts">
